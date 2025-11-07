58cb54bb7f25bafd92855c51684be8b029fccbfa
NoisyDam <Azuria.ee83@gmail.com>
2025-11-07 16:27:42 +0700

    dashboard penjualan

--- dashboard.php
+++ dashboard.php
@@ -1,21 +1,25 @@
 <?php
 session_start();
 
-if( !isset($_SESSION['username']) ) {
-    header("Location: login.php");
-    exit;
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+    exit();
 }
+
+$kode_barang = ["001", "002", "003", "004", "005"];
+$nama_barang = ["Mie ayam Komplit", "Mie Ayam", "Nasi Goreng Komplit", "Pop Mie", "Jus"];
+$harga_barang = [15000, 12000, 20000, 5000, 8000];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Dashboard</title>
 </head>
 <body>
-    <h2>SELAMAT DATANG TUAN <?= $_SESSION['username'];?></h2>
-    <a href="logout.php">Logout</a>
+    <h1>--POLGAN MART--</h1>
+    <p>SELAMAT DATANG TUAN <?php echo $_SESSION['username']; ?>!</p>
+    <a href="logout.php"><button>Logout</button></a>
 </body>
 </html>
